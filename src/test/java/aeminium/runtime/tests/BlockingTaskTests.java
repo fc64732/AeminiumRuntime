b083e9b387ee6a762305812866b97b55d76d8b34
fc64732 <dimelo1906@gmail.com>
2025-11-12 12:08:30 +0000

    Adiciona teste que falha sempre

--- src/test/java/aeminium/runtime/tests/BlockingTaskTests.java
+++ src/test/java/aeminium/runtime/tests/BlockingTaskTests.java
@@ -96,8 +96,9 @@ public class BlockingTaskTests extends BaseTest {
 		rt.shutdown();
 		assertTrue( counter.get() == 2*TASK_COUNT );
 	}
+	@Test
+	public void testThatAlwaysFails() {
+		assertTrue(false);
+	}
 }
-@Test
-public void testThatAlwaysFails() {
-assertTrue(false);
-}
+
